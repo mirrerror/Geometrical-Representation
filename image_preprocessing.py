44ea73c6da3db29fdd6e3c883fd52e3c2250a73a
mirrerror <cayote20@mail.ru>
2023-04-18 16:06:10 +0300

    created the image preprocessing functions

---
+++ image_preprocessing.py
@@ -0,0 +1,29 @@
+import cv2
+
+
+def grayscale_conversion():
+    pass
+
+
+def resize():
+    pass
+
+
+def crop():
+    pass
+
+
+def detect_edge():
+    pass
+
+
+def thresholding():
+    pass
+
+
+def contrast_adjustment():
+    pass
+
+
+def smoothing():
+    pass
