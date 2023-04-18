168ec20fc39512fc1f0606a2f957390a597090a9
mirrerror <cayote20@mail.ru>
2023-04-18 16:30:55 +0300

    added image preprocessing algorithms

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -1,16 +1,35 @@
 import cv2
 
 
-def grayscale_conversion():
-    pass
+def grayscale_conversion(img, save_to_file=False, output_file_name='grayscale.png'):
+    # Convert the image to grayscale
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    if save_to_file:
+        # Save the grayscale image
+        cv2.imwrite(output_file_name, gray_img)
+    return gray_img
 
 
-def resize():
-    pass
+def resize(img, new_size, save_to_file=False, output_file_name='resized.png'):
+    # Resize the image
+    resized_img = cv2.resize(img, new_size, interpolation=cv2.INTER_AREA)
+
+    if save_to_file:
+        # Save the resized image
+        cv2.imwrite(output_file_name, resized_img)
+    return resized_img
 
 
-def crop():
-    pass
+def crop(img, top_left, bottom_right, save_to_file=False, output_file_name='cropped.png'):
+    # Set the coordinates of the top-left corner and bottom-right corner of the desired crop area
+    # Crop the image
+    cropped_img = img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+
+    if save_to_file:
+        # Save the cropped image
+        cv2.imwrite(output_file_name, cropped_img)
+    return cropped_img
 
 
 def detect_edge():
