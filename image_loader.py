b8acf0a41ef98807ee140d529cab11c2947acf7f
mirrerror <cayote20@mail.ru>
2023-04-18 16:01:56 +0300

    added image loading logic

--- image_loader.py
+++ image_loader.py
@@ -1,11 +1,16 @@
 import cv2
 
-# Load the image
-img = cv2.imread('mona_lisa.png')
 
-# Display the image in a window
-cv2.imshow('Image', img)
-cv2.waitKey(0)
+def load_image(path: str):
+    # Load the image
+    img = cv2.imread(path)
+    return img
 
-# Close the window
-cv2.destroyAllWindows()
+
+def show_image(img):
+    # Display the image in a window
+    cv2.imshow('Image', img)
+    cv2.waitKey(0)
+
+    # Close the window
+    cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import image_loader as img_loader
 
-
-print('test')
+img = img_loader.load_image('mona_lisa.png')
+img_loader.show_image(img)
