086fa81b4f8cb004c73e786dfea54cf28e456bbc
LiviuTofan <liviutofan@mail.ru>
2023-04-18 16:59:20 +0300

    functia edge; conrast; thresholding part 2

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -1,48 +1,73 @@
 import cv2
+from image_loader import load_image
+input_image = load_image('mona_lisa.png')
+def grayscale_conversion():
+    pass
 
 
-def grayscale_conversion(img, save_to_file=False, output_file_name='grayscale.png'):
-    # Convert the image to grayscale
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    if save_to_file:
-        # Save the grayscale image
-        cv2.imwrite(output_file_name, gray_img)
-    return gray_img
+def resize():
+    pass
 
 
-def resize(img, new_size, save_to_file=False, output_file_name='resized.png'):
-    # Resize the image
-    resized_img = cv2.resize(img, new_size, interpolation=cv2.INTER_AREA)
-
-    if save_to_file:
-        # Save the resized image
-        cv2.imwrite(output_file_name, resized_img)
-    return resized_img
-
-
-def crop(img, top_left, bottom_right, save_to_file=False, output_file_name='cropped.png'):
-    # Set the coordinates of the top-left corner and bottom-right corner of the desired crop area
-    # Crop the image
-    cropped_img = img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
-
-    if save_to_file:
-        # Save the cropped image
-        cv2.imwrite(output_file_name, cropped_img)
-    return cropped_img
+def crop():
+    pass
 
 
 def detect_edge():
-    pass
+def detect_edge(img):
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    # Apply Gaussian blur to the image to reduce noise
+    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+
+    # Apply Canny edge detection to the image
+    edges = cv2.Canny(blurred, 100, 200)
+
+    # Return the edge map
+    return edges
+    pass
+# Detect edges in the image
+edge_map = detect_edge(input_image)
+
+# Display the edge map in a window
+cv2.imshow('Edges', edge_map)
+cv2.waitKey(0)
+
+def thresholding(img, threshold_value):
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 def thresholding():
-    pass
+    # Apply thresholding to the image
+    ret, thresh = cv2.threshold(gray, threshold_value, 255, cv2.THRESH_BINARY)
 
+    # Return the binary image
+    return thresh
+    pass
+thresholded = thresholding(input_image, 128)
+# Display the thresholded image in a window
+cv2.imshow('Thresholded', thresholded)
+cv2.waitKey(0)
+
+def contrast_adjustment(img):
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 def contrast_adjustment():
+    # Apply histogram equalization to the image
+    equalized = cv2.equalizeHist(gray)
+
+    # Return the equalized image
+    return equalized
     pass
 
+# Perform contrast adjustment on the image
+contrast_adjusted = contrast_adjustment(input_image)
+# Display the adjusted image in a window
+cv2.imshow('Contrast Adjusted', contrast_adjusted)
+cv2.waitKey(0)
 
 def smoothing():
     pass
+
