5dd7846902784f606fc63e29f40073b391876a7f
mirrerror <cayote20@mail.ru>
2023-04-19 16:12:45 +0300

    added initial gui to work with selection of primitives

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import image_loader as img_loader
-import image_preprocessing as img_preproc
+# import image_loader as img_loader
+# import image_preprocessing as img_preproc
+#
+# img = img_loader.load_image('mona_lisa.png')
+# img_loader.show_image(img)
+#
+# img_loader.show_image(img_preproc.grayscale_conversion(img, True))
+#
+# new_size = (200, 350)
+# img_loader.show_image(img_preproc.resize(img, new_size, True))
+#
+# top_left = (0, 0)
+# bottom_right = (100, 300)
+# img_loader.show_image(img_preproc.crop(img, top_left, bottom_right, True))
+#
+# img_loader.show_image(img_preproc.thresholding(img, 128, True))
+# img_loader.show_image(img_preproc.contrast_adjustment(img, True))
+# img_loader.show_image(img_preproc.detect_edges(img, True))
+# img_loader.show_image(img_preproc.gaussian_blur(img, True))
+import primitive_choosing
 
-img = img_loader.load_image('mona_lisa.png')
-img_loader.show_image(img)
-
-img_loader.show_image(img_preproc.grayscale_conversion(img, True))
-
-new_size = (200, 350)
-img_loader.show_image(img_preproc.resize(img, new_size, True))
-
-top_left = (0, 0)
-bottom_right = (100, 300)
-img_loader.show_image(img_preproc.crop(img, top_left, bottom_right, True))
-
-img_loader.show_image(img_preproc.thresholding(img, 128, True))
-img_loader.show_image(img_preproc.contrast_adjustment(img, True))
-img_loader.show_image(img_preproc.detect_edges(img, True))
-img_loader.show_image(img_preproc.gaussian_blur(img, True))
+primitive_choosing.start_gui('mona_lisa.png')
--- primitive_choosing.py
+++ primitive_choosing.py
@@ -3,13 +3,33 @@ import numpy as np
 import tkinter as tk
 from tkinter import ttk
 
+from PIL import Image, ImageTk
+
+import image_loader
+
 
 def on_shape_selected(event):
     global selected_shape
     # This way we store the shape we have selected in a variable: selected_shape
     selected_shape = shape_combobox.get()
     print(f"Selected shape: {selected_shape}")
-    draw_shape(selected_shape)
+    set_image_displayed(draw_shape(selected_shape))
+
+
+def set_image_displayed(img):
+    global img_tk
+
+    # Convert the image to the correct format
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Create a Tkinter PhotoImage object
+    img = Image.fromarray(image)
+    img_tk = ImageTk.PhotoImage(image=img)
+
+    # Create a Tkinter Canvas widget
+    canvas = tk.Canvas(root, width=img.width, height=img.height)
+    canvas.create_image(0, 0, anchor="nw", image=img_tk)
+    canvas.grid(row=1, column=0)
 
 
 def draw_shape(shape):
@@ -28,35 +48,40 @@ def draw_shape(shape):
     elif shape == "Ellipse":
         img = cv2.ellipse(img, center, (15, 10), 0, 0, 360, color, 2)
 
-    # Display the image with the drawn shape
-    cv2.imshow("Shape", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    return img
 
-
-# Read the input image
-input_image = cv2.imread("ex0.png")
 
 # I think about sticking to these figures for now
 shapes = ["Triangle", "Circle", "Rectangle", "Ellipse"]
 
 selected_shape = None
+shape_combobox = None
+root = None
+input_image = None
 
-# Creating the small window where we can select the shape:
-root = tk.Tk()
-root.title("Primitive Shape Selector")
 
-frame = ttk.Frame(root, padding="10")
-frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S)) # sticky makes the text stretch in all directions
+def start_gui(input_image_file_name: str):
+    global shape_combobox, root, input_image
 
-shape_label = ttk.Label(frame, text="Choose a primitive shape:")
-shape_label.grid(row=0, column=0, padx=(0, 10), pady=(0, 10), sticky=tk.W)
+    input_image = image_loader.load_image(input_image_file_name)
 
-shape_combobox = ttk.Combobox(frame, values=shapes)
-shape_combobox.grid(row=0, column=1, pady=(0, 10))
-shape_combobox.bind("<<ComboboxSelected>>", on_shape_selected)
+    # Creating the small window where we can select the shape:
+    root = tk.Tk()
+    root.title("Primitive Shape Selector")
 
-# Optional as fuck
-ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=1)
+    frame = ttk.Frame(root, padding="10")
+    frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))  # sticky makes the text stretch in all directions
 
-root.mainloop()
+    shape_label = ttk.Label(frame, text="Choose a primitive shape:")
+    shape_label.grid(row=0, column=0, padx=(0, 10), pady=(0, 10), sticky=tk.W)
+
+    shape_combobox = ttk.Combobox(frame, values=shapes)
+    shape_combobox.grid(row=0, column=1, pady=(0, 10))
+    shape_combobox.bind("<<ComboboxSelected>>", on_shape_selected)
+
+    # Optional as fuck
+    ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=1)
+
+    set_image_displayed(input_image)
+
+    root.mainloop()
